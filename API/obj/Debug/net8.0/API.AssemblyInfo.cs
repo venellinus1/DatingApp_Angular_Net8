2c21230fc753c75d91cb0074da1fdf61e79c9046
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-09-04 18:24:27 +0200

    Adding Homepage and Register form and

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68b3000860981dc61c93246c0aa8815dd29254ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4325e98a9bcdc0e97375a99f85c769c21bec7c0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
