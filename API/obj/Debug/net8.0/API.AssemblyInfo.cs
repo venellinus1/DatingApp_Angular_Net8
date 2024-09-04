467778b8d71265da853292e479d9ce57fbe8b64f
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-09-04 11:47:15 +0200

    Program.cs:  - configuration for authentication  - middleware - add authentication and authorization - order is important
     UsersController - use authorize and allowannonymous attributes for endpoints

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d6151911bcbb3de7dbd9cd187807cf679e5b3eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bcbb5c2b3004377bcd4294d929b96966ab37649")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
