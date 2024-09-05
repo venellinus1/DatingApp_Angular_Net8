21b1bf708538325b416b96b16a793f43f52b68c6
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-09-05 18:09:33 +0200

    Add more components to work on Start working on routing

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c21230fc753c75d91cb0074da1fdf61e79c9046")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+130fe82ae780c1b17bc277a06105966ff8374a42")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
