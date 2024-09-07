fa2cfa773540598851123bc88087e9477cf5a449
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-09-07 17:44:24 +0200

    Test error handling by adding a new Component with test buttons for different errors

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81905e51d1a5cf083e20dff79bf15452552d0a78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7764204404d12ee80da5b96ee3c177f65ccd82f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
