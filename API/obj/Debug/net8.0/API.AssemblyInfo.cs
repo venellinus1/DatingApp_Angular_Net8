8535db104b66b6a6aef9da74d5afb9814f886f18
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-09-09 09:56:26 +0200

    Use Interceptors to handle Errors Add Server error and Not found error components

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7764204404d12ee80da5b96ee3c177f65ccd82f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa2cfa773540598851123bc88087e9477cf5a449")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
