f024bb9b004d398ffcc270ceb005871526b20893
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-09-12 09:17:28 +0200

    Working on UI: -add member service to retrieve list of members or single member -create member Cards, add styling, animated buttons -use Interceptor to send the token -routing to member details page -styling member details page

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { RegisterComponent } from "../register/register.component";
-import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-home',
@@ -9,27 +9,15 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent {
   
   registerMode = false;
-  users: any;
-  http = inject(HttpClient);
   
   registerToggle(){
     this.registerMode = !this.registerMode;
   }
 
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    });
-  }
 
-  ngOnInit(): void {
-    this.getUsers();
-  }
 
   cancelRegisterMode(event: boolean){
     this.registerMode = event;
