9e597ef3490491f5df87f74bcef4bc58952b1279
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-09-18 14:54:09 +0200

    Add Cloudinary settings model and include it in ApplicationServiceExtensions

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+945d589cc195290a0575e843e0e8ad87f5c44b59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2beb7b058d4391ffa69b2458df164d7db72d604")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
