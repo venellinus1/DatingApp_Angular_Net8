f66e704acd98f65fd84b9dd060e904ad49b98792
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-09-18 12:28:11 +0200

    update gitignore

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9da7f521dc0c6ea8bbc0aa32b5235ac027ecd51e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+945d589cc195290a0575e843e0e8ad87f5c44b59")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
