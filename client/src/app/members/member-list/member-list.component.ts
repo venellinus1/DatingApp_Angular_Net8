45129774ca321faab199d87384d13dfaafaa9f3a
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-09-27 17:56:48 +0200

    Client - implement Pagination - using ngx-bootstrap pagination Refactor members.service - wip Refactor member-list.component and template to implement pagination

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,22 +1,32 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { MemberCardComponent } from "../member-card/member-card.component";
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
     selector: 'app-member-list',
     standalone: true,
     templateUrl: './member-list.component.html',
     styleUrl: './member-list.component.css',
-    imports: [MemberCardComponent]
+    imports: [MemberCardComponent, PaginationModule]
 })
 export class MemberListComponent implements OnInit {
   memberService = inject(MembersService);
+  pageNumber = 1;
+  pageSize = 5;
 
   ngOnInit(): void {
-    if (this.memberService.members().length === 0) this.loadMembers();
+    if (!this.memberService.paginatedResult()) this.loadMembers();
   }
 
   loadMembers() {
-    this.memberService.getMembers()
+    this.memberService.getMembers(this.pageNumber, this.pageSize)
+  }
+
+  pageChanged(event: any){
+    if(this.pageNumber !== event.pageChanged){
+      this.pageNumber = event.page;
+      this.loadMembers();
+    }
   }
 }
