c7b1cefe3b805ee5309aadb407f0e22c2a417af1
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-09-28 21:05:12 +0200

    Client - add formating for dates for Matching profiles and for own profile

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bd311a00fd48e0af273aa40537b74724baf9ec3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8044a7db0aed640ea3a6b6cd92ea3ccbb54744d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
