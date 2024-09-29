f889ea8c28558117cbc6f19a49cfc3ff669ce144
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-09-29 15:29:56 +0200

    Working on Likes feature - Add Likes entity - Add Likes repository - Implement Likes repository

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6eee10b310def58e3ed354c1e2eab11519787b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bfd019ba7a1345ed182f1e8b947360f9878ca07")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
