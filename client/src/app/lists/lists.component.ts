4edd209300f31d2d20d3c06ce6310875e387add9
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-09-29 17:01:53 +0200

    Likes Feature - Client - member-card component - add toggleLike method - implement the method in member-card template - adding likes component

--- client/src/app/lists/lists.component.ts
+++ client/src/app/lists/lists.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { LikesService } from '../_services/likes.service';
+import { Member } from '../_models/Member';
+import { FormsModule } from '@angular/forms';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { MemberCardComponent } from "../members/member-card/member-card.component";
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ButtonsModule, MemberCardComponent],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
-export class ListsComponent {
+export class ListsComponent implements OnInit{
 
+  private likesService = inject(LikesService);
+  members: Member[] = [];
+  predicate = 'liked';
+
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  getTitle(){
+    switch(this.predicate){
+      case 'liked': return 'Members you like';
+      case 'likedBy': return 'Members who like you';
+      default: return 'Mutual';
+    }
+  }
+
+  loadLikes(){
+    this.likesService.getLikes(this.predicate).subscribe({
+      next: members => this.members = members,
+    })
+  }
 }
