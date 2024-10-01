a35e8ef3437427c555252ca429078f676c2251bf
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-10-01 09:11:05 +0200

    Msgs Feature - Client - send messages - Add sendMessage() in message.service - member.detail.component - onUpdateMessage - update messages on message event - member-messages.component - updated form to use ViewChild and template variable

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+185b28c60bb1e01cdf5362d6735d6b41313d5df4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d242a118867915befb55c8180ccc087c52b0681b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,24 +1,31 @@
-import { Component, inject, input, OnInit } from '@angular/core';
+import { Component, inject, input, OnInit, output, ViewChild } from '@angular/core';
 import { MessageService } from '../../_services/message.service';
 import { Message } from '../../_models/Message';
 import { TimeagoModule } from 'ngx-timeago';
+import { FormsModule, NgForm } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-member-messages',
   standalone: true,
-  imports: [TimeagoModule],
+  imports: [TimeagoModule, FormsModule],
   templateUrl: './member-messages.component.html',
   styleUrl: './member-messages.component.css'
 })
-export class MemberMessagesComponent implements OnInit {
+export class MemberMessagesComponent {
+  @ViewChild('messageForm') messageForm?: NgForm;
   username = input.required<string>();
-  //private messageService = inject(MessageService);
-
+  private messageService = inject(MessageService);
+  messageContent = '';
   messages = input.required<Message[]>();
+  updateMessages = output<Message>();
   
-
-  ngOnInit(): void {
-    
+  sendMessage(){
+    this.messageService.sendMessage(this.username(), this.messageContent).subscribe({
+      next: message => {
+        this.updateMessages.emit(message);
+        this.messageForm?.reset();
+      }
+    })
   }
-
 }
