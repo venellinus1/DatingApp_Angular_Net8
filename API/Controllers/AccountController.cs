29db388c5d8cfa10013e85dc942b58bdbd5fab08
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-10-01 15:00:26 +0200

    Identity - update the AccountController to match the use of Identity changes Update the Seed method to include Admin user, set Users roles

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,28 +1,26 @@
-using System.Security.AccessControl;
-using System.Security.Cryptography;
-using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
 using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(DataContext context, ITokenService tokenService, IMapper mapper) : BaseApiController
+public class AccountController(UserManager<AppUser> userManager, ITokenService tokenService, IMapper mapper) : BaseApiController
 {
     [HttpPost("register")] // account/register
     public async Task<ActionResult<UserDTO>> Register(RegisterDto registerDto)
     {
         if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
-        using var hmac = new HMACSHA512();
         var user = mapper.Map<AppUser>(registerDto);
         user.UserName = registerDto.Username.ToLower();
-        
-        context.Users.Add(user);
-        await context.SaveChangesAsync();
+
+        var result = await userManager.CreateAsync(user, registerDto.Password);
+
+        if (!result.Succeeded) return BadRequest(result.Errors);
         
         return new UserDTO
         {
@@ -36,14 +34,16 @@ public class AccountController(DataContext context, ITokenService tokenService, 
     [HttpPost("login")]
     public async Task<ActionResult<UserDTO>> Login(LoginDto loginDto)
     {
-        var user = await context.Users
+        var user = await userManager.Users
             .Include(p => p.Photos)
             .FirstOrDefaultAsync(x => 
-                x.UserName == loginDto.Username.ToLower());
+                x.NormalizedUserName == loginDto.Username.ToUpper());
 
         if (user == null || user.UserName == null) return Unauthorized("Invalid Username");
 
-        
+        var result = await userManager.CheckPasswordAsync(user, loginDto.Password);
+        if (!result) return Unauthorized();//otherwise any password will be accepted!
+
         return new UserDTO
         {
             Username = user.UserName,
@@ -56,6 +56,6 @@ public class AccountController(DataContext context, ITokenService tokenService, 
 
     private async Task<bool> UserExists(string username)
     {
-        return await context.Users.AnyAsync(x => x.NormalizedUserName == username.ToUpper());// 
+        return await userManager.Users.AnyAsync(x => x.NormalizedUserName == username.ToUpper());// 
     }
 }
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1361013532bcea4dbb93f489c9194dbd723301c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b710f33f3d74a5bd4dac5234c722afc60c78552f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
