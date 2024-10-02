887bc3959ade22b10d0f156ef61753f326453d05
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-10-02 12:13:10 +0200

    SignalR - Client - send message -refactor message.service to replace the API send messages with SignalR Hub -refactor member-messages.component

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -32,6 +32,10 @@ export class MessageService {
     this.hubConnection.on('ReceiveMessageThread', messages => {
       this.messageThread.set(messages);
     })
+
+    this.hubConnection.on('NewMessage', message => {
+      this.messageThread.update(messages => [...messages, message]);
+    })
   }
 
   stopHubConnection(){
@@ -54,8 +58,9 @@ export class MessageService {
     return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + username);
   }
 
-  sendMessage(username: string, content: string){
-    return this.http.post<Message>(this.baseUrl + 'messages', {recipientUsername: username, content});
+  async sendMessage(username: string, content: string){//use async to avoid returning Promise<> | undefined - with async its just Promise<>
+    // SendMessage should match the name in MessageHub.cs
+    return this.hubConnection?.invoke('SendMessage', {recipientUsername: username, content}); 
   }
 
   deleteMessage(id: number){
--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -18,10 +18,8 @@ export class MemberMessagesComponent {
   messageContent = '';  
   
   sendMessage(){
-    this.messageService.sendMessage(this.username(), this.messageContent).subscribe({
-      next: message => {
-        this.messageForm?.reset();
-      }
+    this.messageService.sendMessage(this.username(), this.messageContent).then(() => {
+      this.messageForm?.reset();
     })
   }
 }
